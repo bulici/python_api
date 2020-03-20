e5a9f5cf043f9f6b488b29c0264acb552c723cf1
bulici <294666094@qq.com>
2020-03-20 14:02:54 +0800

    修改了注释

--- api_run.py
+++ api_run.py
@@ -6,9 +6,6 @@ Email : 294666094@qq.com
 Motto : Clumsy birds have to start flying early.
 =================================================
 """
-"""
-1、完成项目其他接口用例的编写和测试用例类的编写
-"""
 import unittest
 import os
 from HTMLTestRunnerNew import HTMLTestRunner
@@ -26,25 +23,25 @@ suite.addTest(loader.discover(TESTCASEDIR))
 # from testcase import test11mainstreaming
 # suite.addTest(loader.loadTestsFromModule(test11mainstreaming))
 
-#创建用例运行程序并执行用例
-# file_path = os.path.join(REPORTDIR,'report.html')
-# runner = HTMLTestRunner(stream=open(file_path,'wb'),
-#                         title='接口自动化测试报告',
-#                         description='报告描述：前程贷接口自动化测试的报告。',
-#                         tester='布里茨')
-# runner.run(suite)
-
 import datetime
 #创建一个时间格式
 date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
+#创建用例运行程序并执行用例
+file_path = os.path.join(REPORTDIR,'report_{}.html'.format(date))
+runner = HTMLTestRunner(stream=open(file_path,'wb'),
+                        title='接口自动化测试报告',
+                        description='报告描述：前程贷接口自动化测试的报告。',
+                        tester='布里茨')
+runner.run(suite)
+
 #另外一种测试报告:BeautifulReport
-from BeautifulReport import BeautifulReport
-file_path = os.path.join(REPORTDIR,'{}_接口自动化测试报告.html'.format(date))
-br_runner = BeautifulReport(suite)
-br_runner.report(description='报告描述：前程贷接口自动化测试的报告。',
-                 filename='{}_接口自动化测试报告.html'.format(date),
-                 report_dir=REPORTDIR)
+# from BeautifulReport import BeautifulReport
+# file_path = os.path.join(REPORTDIR,'{}_接口自动化测试报告.html'.format(date))
+# br_runner = BeautifulReport(suite)
+# br_runner.report(description='报告描述：前程贷接口自动化测试的报告。',
+#                  filename='{}_接口自动化测试报告.html'.format(date),
+#                  report_dir=REPORTDIR)
 
 #发送测试报告邮件至邮箱
 text_msg = "请下载附件查看详细接口测试报告内容！"

39835f876967135289bdb9e5742754b42c8ce354
bulici <294666094@qq.com>
2020-03-26 21:41:02 +0800

    测试报告格式修改

--- api_run.py
+++ api_run.py
@@ -23,12 +23,12 @@ suite.addTest(loader.discover(TESTCASEDIR))
 # from testcase import test07mainstreaming
 # suite.addTest(loader.loadTestsFromModule(test11mainstreaming))
 
-import datetime
+# import datetime
 #创建一个时间格式
-date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+# date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
 #创建用例运行程序并执行用例
-file_path = os.path.join(REPORTDIR,'report_{}.html'.format(date))
+file_path = os.path.join(REPORTDIR,'report.html')
 runner = HTMLTestRunner(stream=open(file_path,'wb'),
                         title='接口自动化测试报告',
                         description='报告描述：测试开发平台接口自动化测试的报告。',
